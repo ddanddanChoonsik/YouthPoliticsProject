bb769e0086c9f72a50fc7692d187d970cdde103b
ddanddan <tldus0106@naver.com>
2023-02-04 15:31:03 +0900

    pushing test version all 1.0

---
+++ YouthPoliticsProject/src/main/java/project/home/study/YouthPoliticsProjectApplication.java
@@ -0,0 +1,13 @@
+package project.home.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouthPoliticsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouthPoliticsProjectApplication.class, args);
+	}
+
+}
---
+++ YouthPoliticsProject/src/test/java/project/home/study/YouthPoliticsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.home.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouthPoliticsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
