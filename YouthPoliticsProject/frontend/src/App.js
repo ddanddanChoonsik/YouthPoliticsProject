fa16d3481b42829549a8f866f6da6205d17b585b
ddanddan <tldus0106@naver.com>
2023-03-08 01:40:24 +0900

    pom.xml spring boot version error (3.@@ 부터는 javase11안됨) 어중떠중 api 오류는 고침 불러오기 fail

--- YouthPoliticsProject/frontend/src/App.js
+++ YouthPoliticsProject/frontend/src/App.js
@@ -1,9 +1,46 @@
+import React, { useEffect,useState} from 'react';
 import { BrowserRouter } from "react-router-dom";
 import RouteMain from "./RouteMain";
 import { Menu,Main } from "./components";
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  //0.1
+  
+  useEffect(() => {
+  async function fetchdata() {
+    try{
+    const res  = await axios.get(
+      '?pageIndex=1&display=1&openApiVlak=c4ef1792d2b792033d1e4126',
+      );
+      console.log("json:",res);
+    
+  }catch (e) {
+  console.log("e:",e)
+}
+}
+  fetchdata();
+}, []);
+
+
+//0.2 
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const res = await axios.get(
+//         `?pageIndex=1&display=1&openApiVlak=c4ef1792d2b792033d1e4126`
+//       )
+//       console.log("json:",res.data.response.body.items.item)
+//     } catch (e) {
+//       console.log("e:",e)
+//     }
+//   }
+//   fetchData()
+// }, [])
+
   return (
     <BrowserRouter>
       <Menu/>
--- YouthPoliticsProject/frontend/src/components/Main.js
+++ YouthPoliticsProject/frontend/src/components/Main.js
@@ -5,70 +5,35 @@ import axios from 'axios';
 
 const Main = () => {
     //청년정책 api key
-    //const youthapikey=process.env.REACT_APP_YOUTH_POLITICS_API_KEY_SY;
-    //const youthapikey="c4ef1792d2b792033d1e4126";
+    // const openApiVlak= "c4ef1792d2b792033d1e4126";
+    // const pageIndex=1;
+    // const display=1;
+    // const url=`https://www.youthcenter.go.kr/opi/empList.do?pageIndex=${pageIndex}&display=${display}&openApiVlak=${openApiVlak}`;
+    // console.log("url:",url); //값 들어옴
 
-    //let apiURL=`https://www.youthcenter.go.kr/opi/empList.do?pageIndex=1&display=2&query=주거&openApiVlak=${youthapikey}&srchPolyBizSecd=003002001`;   
+    // axios.get(url).then((Response)=>{
+    //     const data = Response.data;
+    //     console.log("data:",data);
+    // })
 
-    // useEffect(() => {
-    //     const config = {
-    //         headers: {
-    //             'Accept': 'application/json'
-    //         }
-    //     }
-    //     fetch('/www.youthcenter.go.kr/opi/empList.do?pageIndex=1&display=1&query=%EC%A3%BC%EA%B1%B0&openApiVlak=c4ef1792d2b792033d1e4126&srchPolyBizSecd=003002001', config)
-    //          .then((res) => console.log("response:", res.json()))
-    //          .catch((error) => console.log("error:", error));
-    // }, [])
-
-    ///fail0.3
-    // const [content, setContent] = useState([]);
-
-    // useEffect(() => {
-    //     fetch('https://www.youthcenter.go.kr/opi/empList.do?pageIndex=1&display=1&query=%EC%A3%BC%EA%B1%B0&openApiVlak=c4ef1792d2b792033d1e4126&srchPolyBizSecd=003002001').then(
-    //       response=>{
-    //         response.headers("Access-Control-Allow-Origin","*")
-    //           return response.json();
-    //       }
-    //       ).then(data=>{
-    //           setContent(data);
-    //           console.log("api:",data);
-    //       }
-    //     )}, []);
-    //0.4
-    // const getData = async () => {
-    //     const res = await fetch(
-    //       "https://jsonplaceholder.typicode.com/comments"
-    //     ).then((res) => res.json());
-    //     console.log(res);
-    //   };
-
-    //0.5
-    // const URL = "https://www.youthcenter.go.kr/opi/empList.do?";
-
-    // const response = axios.get(URL, {
-    //     params: {
-    //       openApiVlak: process.env.REACT_APP_YOUTH_POLITICS_API_KEY_SY,
-    //       pageIndex: 1,
-    //       display: 1
-    //     }
-    // });
-
-    //0.6
-    // const getData = async () =>{
-    //     const res = await axios.get('')
-    // }
-    const openApiVlak= "c4ef1792d2b792033d1e4126";
-    const pageIndex=1;
-    const display=1;
-    const url=`https://www.youthcenter.go.kr/opi/empList.do?pageIndex=${pageIndex}&display=${display}&openApiVlak=${openApiVlak}`;
-    console.log("url:",url); //값 들어옴
-
-    axios.get(url).then((Response)=>{
-        const data = Response.data;
-        console.log("data:",data);
-    })
-
+    // const express = require('express');
+    // const cors = require('cors');
+  
+    // let corsOption = {
+    //     origin: 'http://localhost:3000' // 허락하는 요청 주소
+    //     credentials: true // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+    // } 
+  
+    // app.use(cors(corsOption)); // CORS 미들웨어 추가
+    useEffect(() => {
+        async function fetchdata() {
+          const { data } = await axios.get(
+            '?pageIndex=1&display=1&openApiVlak=c4ef1792d2b792033d1e4126',
+            );
+          console.log(data);
+        }
+        fetchdata();
+      }, []);
     return (
         <div id='main'>
             <div className='mainContent'>
