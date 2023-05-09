3ea30c0786661b5fb7afe0d7bdacf2ec2654e272
peterjy99 <hjywns1628@gmail.com>
2023-05-09 17:48:47 +0900

    test push

--- YouthPoliticsProject/frontend/src/pages/login/LoginForm.js
+++ YouthPoliticsProject/frontend/src/pages/login/LoginForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../../styles/loginpage.css';
 
 const LoginForm = () => {
-    return (
-        <div>
-            <p>일반로그인</p>
-        </div>
-    );
+  return (
+      <div>
+          <p>일반로그인</p>
+      </div>
+  );
 };
 
 export default LoginForm;
