bf605d0ae4dee57680c1a4761f40d54d51507362
peterjy99 <hjywns1628@gmail.com>
2023-05-12 19:50:17 +0900

    aaaaaaaa

--- YouthPoliticsProject/frontend/src/pages/login/LoginForm.js
+++ YouthPoliticsProject/frontend/src/pages/login/LoginForm.js
@@ -1,11 +1,58 @@
-import React from 'react';
-import '../../styles/loginpage.css';
+import React from "react";
+import { HashRouter as Router, Route, NavLink } from "react-router-dom";
+import SignUpForm from "./SignUpForm";
+import SignInForm from "./SignInForm";
+
+import "./LoginForm.css";
 
 const LoginForm = () => {
   return (
-      <div>
-          <p className='test'>일반로그인</p>
+    <Router basename="/react-auth-ui/">
+      <div className="LoginForm">
+        <div className="formAside" />
+        <div className="form">
+          <div className="pageSwitcher">
+            <NavLink
+              to="/sign-in"
+              activeClassName="pageSwitcherItem-active"
+              className="pageSwitcherItem"
+            >
+              Sign In
+            </NavLink>
+            <NavLink
+              exact
+              to="/"
+              activeClassName="pageSwitcherItem-active"
+              className="pageSwitcherItem"
+            >
+              Sign Up
+            </NavLink>
+          </div>
+
+          <div className="formTitle">
+            <NavLink
+              to="/sign-in"
+              activeClassName="formTitleLink-active"
+              className="formTitleLink"
+            >
+              Sign In
+            </NavLink>{" "}
+            or{" "}
+            <NavLink
+              exact
+              to="/"
+              activeClassName="formTitleLink-active"
+              className="formTitleLink"
+            >
+              Sign Up
+            </NavLink>
+          </div>
+
+          <Route exact path="/" component={SignUpForm} />
+          <Route path="/sign-in" component={SignInForm} />
+        </div>
       </div>
+    </Router>
   );
 };
 
